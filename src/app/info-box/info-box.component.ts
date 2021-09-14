e3c806bccc00be0050f4b4e485c9098ade22a355
GetTuh <jakubniemczyk97@gmail.com>
2021-09-15 00:47:37 +0200

    :sparkles: new data display

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -6,16 +6,29 @@ import { Component, Input ,OnChanges, SimpleChanges} from '@angular/core';
   styleUrls: ['./info-box.component.scss']
   
 })
+
 export class InfoBoxComponent implements OnChanges {
-  info='loading'
+  info:any
+  main:string | undefined
+  icon:string|undefined
   @Input() weather:any
   ngOnChanges(changes:SimpleChanges){
-    try {
-      this.info=this.weather.name
-    }
-    catch(error){
-      this.info='loading'
+      this.info=this.weather
+      this.main=this.adjective(this.info.weather[0].main)
+      this.icon=`http://openweathermap.org/img/wn/${this.info.weather[0].icon}.png`
+  }
+  adjective=(main:string)=>{
+    switch (main.toLowerCase()) {
+      case 'fog': return 'foggy';
+      case 'clear': return 'bright';
+      case 'rain': return 'rainy';
+      case 'sun': return 'sunny';
+      case 'snow': return 'snowy';
+      case 'clouds':return 'cloudy'
+      default:return main
     }
   }
+  
+  
 }
 

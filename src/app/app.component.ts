3eb36ccbe9560d92c82c87fcc192d9cbe633deda
GetTuh <jakubniemczyk97@gmail.com>
2021-09-15 00:08:23 +0200

    :construction: Searching conditional display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ import { environment } from 'src/environments/environment';
 
 export class AppComponent  {
   title:any = 'MachineTestAngular';
+  loading:boolean=false
   weatherData:any
   displayNameIfLoaded: any=()=>{try{return this.weatherData.name}catch(e){return 0}}
   cityName:string | undefined='';
   getWeatherData=async(city:string)=>{
+    this.loading=true
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${environment.OPENWEATHER_API_KEY}`
     this.weatherData = await (await fetch(url)).json()
   }
